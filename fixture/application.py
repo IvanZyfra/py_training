22c20fbf8ab1c0e0ad6343ad6cea341170e784c8
Ivan Polyakov <ivan.polyakov@zyfra.com>
2022-02-08 21:27:09 +0300

    Задание_5(выделен класс для работы с контактами)

--- fixture/application.py
+++ fixture/application.py
@@ -1,9 +1,9 @@
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-from selenium.webdriver.support.ui import Select
 from fixture.session import SessionHelper
 from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 
 class Application:
@@ -13,72 +13,8 @@ class Application:
         self.wd.implicitly_wait(30)
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
-
-    def add_new_contact(self, contact):
-        wd = self.wd
-        # Нажимаем кнопку "Добавить контакт"
-        wd.find_element_by_link_text("add new").click()
-        # Заполняем поля
-        wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").send_keys(contact.first_name)
-        wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").send_keys(contact.middle_name)
-        wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").send_keys(contact.last_name)
-        wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").send_keys(contact.nickname)
-        wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").send_keys(contact.title)
-        wd.find_element_by_name("theform").click()
-        wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").send_keys(contact.company)
-        wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").send_keys(contact.address)
-        wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").send_keys(contact.tel_home)
-        wd.find_element_by_name("mobile").send_keys(contact.tel_mobile)
-        wd.find_element_by_name("work").send_keys(contact.tel_work)
-        wd.find_element_by_name("fax").send_keys(contact.tel_fax)
-        wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").send_keys(contact.email)
-        wd.find_element_by_name("email2").send_keys(contact.email2)
-        wd.find_element_by_name("email3").send_keys(contact.email3)
-        wd.find_element_by_name("homepage").click()
-        wd.find_element_by_name("homepage").send_keys(contact.homepage)
-        # работа с выпадающими списками
-        wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bday")).select_by_visible_text("1")
-        wd.find_element_by_xpath("//option[@value='1']").click()
-        wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("December")
-        wd.find_element_by_xpath("//option[@value='December']").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").send_keys("2000")
-        wd.find_element_by_name("aday").click()
-        Select(wd.find_element_by_name("aday")).select_by_visible_text("1")
-        wd.find_element_by_xpath("//div[@id='content']/form/select[3]/option[3]").click()
-        wd.find_element_by_name("amonth").click()
-        Select(wd.find_element_by_name("amonth")).select_by_visible_text("September")
-        wd.find_element_by_xpath("//div[@id='content']/form/select[4]/option[10]").click()
-        wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("ayear").click()
-        wd.find_element_by_name("ayear").send_keys("2010")
-        # Заполнение доп. полей
-        wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").send_keys(contact.address2)
-        wd.find_element_by_name("phone2").click()
-        wd.find_element_by_name("phone2").send_keys(contact.phone2)
-        wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").send_keys(contact.notes)
-        # sumbit contact
-        wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
-        self.return_to_home_page()
-
-
-    def return_to_home_page(self):
-        wd = self.wd
-        wd.find_element_by_link_text("home page").click()
 
     def open_home_page(self):
         wd = self.wd
--- test/test_add_new_contact.py
+++ test/test_add_new_contact.py
@@ -12,6 +12,6 @@ def app(request):
     
 def test_add_new_contact(app):
     app.session.login(username="admin", password="secret")
-    app.add_new_contact(Contact(first_name="Fname", middle_name="Mname", last_name="Lname", nickname="Nickname", title="Test_title", company="Test_company", address="Test_address", tel_home="123456", tel_mobile="11223344", tel_work="11224433", tel_fax="11225566", email="test@mail.com", email2="test2@mail.com", email3="test3@mail.com", homepage="www.test.com", address2="second_address", phone2="123465798", notes="test_notes"))
+    app.contact.add_new_contact(Contact(first_name="Fname", middle_name="Mname", last_name="Lname", nickname="Nickname", title="Test_title", company="Test_company", address="Test_address", tel_home="123456", tel_mobile="11223344", tel_work="11224433", tel_fax="11225566", email="test@mail.com", email2="test2@mail.com", email3="test3@mail.com", homepage="www.test.com", address2="second_address", phone2="123465798", notes="test_notes"))
     app.session.logout()
 
