d7c9571819f1f1ec23712b2f36e56993446ee32a
Ivan Polyakov <ivan.polyakov@zyfra.com>
2022-02-10 21:31:21 +0300

    Рефактор теста модификации группы

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -4,7 +4,7 @@ from model.contact import Contact
 
 def test_modify_contact(app):
     app.session.login(username="admin", password="secret")
-    app.contact.modify(
+    app.contact.modify_first_group(
         Contact(first_name="Test_name", middle_name="Test_name", last_name="Test_name", nickname="", title="",
                 company="", address="", tel_home="", tel_mobile="",
                 tel_work="", tel_fax="", email="", email2="",
