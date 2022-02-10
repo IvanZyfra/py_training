3447a7a520f5220665a35b336d2117eecb6b822d
Ivan Polyakov <ivan.polyakov@zyfra.com>
2022-02-10 22:47:08 +0300

    Улучшена интеллектуальность фикстуры

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -3,11 +3,9 @@ from model.contact import Contact
 
 
 def test_modify_contact(app):
-    app.session.login(username="admin", password="secret")
     app.contact.modify_first_group(
         Contact(first_name="Test_name", middle_name="Test_name", last_name="Test_name", nickname="", title="",
                 company="", address="", tel_home="", tel_mobile="",
                 tel_work="", tel_fax="", email="", email2="",
                 email3="", homepage="", address2="", phone2="",
                 notes=""))
-    app.session.logout()
--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,12 +3,8 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(name="test_test"))
-    app.session.logout()
 
 
 def test_modify_group_header(app):
-    app.session.login(username="admin", password="secret")
     app.group.modify_first_group(Group(header="test_header"))
-    app.session.logout()
