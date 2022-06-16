58a8acd8898fc2b4ef74281ddf22979ed7c1349a
Ivan Polyakov <ivan.polyakov@zyfra.com>
2022-06-16 12:08:50 +0300

    Revert "Revert "Revert "Задание 13 (Реализованно случайное удаление элемента из списка контактов и групп)"""
    This reverts commit bfd0e3e0cdbe92d96c8f324a85ce82b57730a3f2.

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,9 +1,8 @@
 from model.contact import Contact
-from random import randrange
 
 
 
-def test_delete_some_contact(app):
+def test_delete_first_contact(app):
     if app.contact.count() == 0:
         app.contact.add_new_contact(Contact(first_name="Test", middle_name="Test", last_name="Test_name", nickname="", title="",
                 company="", address="", tel_home="", tel_mobile="",
@@ -11,9 +10,8 @@ def test_delete_some_contact(app):
                 email3="", homepage="", address2="", phone2="",
                 notes=""))
     old_contacts = app.contact.get_contacts_list()
-    index = randrange(len(old_contacts))
-    app.contact.delete_contact_by_index(index)
+    app.contact.delete_first_contact()
     assert len(old_contacts) - 1 == app.contact.count()
     new_contacts = app.contact.get_contacts_list()
-    old_contacts[index:index+1] = []
+    old_contacts[0:1] = []
     assert old_contacts == new_contacts
--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 from model.contact import Contact
-from random import randrange
-
 
 
 def test_modify_contact(app):
@@ -12,11 +10,10 @@ def test_modify_contact(app):
                 email3="", homepage="", address2="", phone2="",
                 notes=""))
     old_contacts = app.contact.get_contacts_list()
-    index = randrange(len(old_contacts))
     contact = Contact(first_name="Fname", middle_name="Mname", last_name="Lname", nickname="Nickname", title="Test_title", company="Test_company", address="Test_address", tel_home="123456", tel_mobile="11223344", tel_work="11224433", tel_fax="11225566", email="test@mail.com", email2="test2@mail.com", email3="test3@mail.com", homepage="www.test.com", address2="second_address", phone2="123465798", notes="test_notes")
-    contact.id = old_contacts[index].id
-    app.contact.modify_contact_by_index(contact, index)
+    contact.id = old_contacts[0].id
+    app.contact.modify_contact(contact)
     assert len(old_contacts) == app.contact.count()
     new_contacts = app.contact.get_contacts_list()
-    old_contacts[index] = contact
+    old_contacts[0] = contact
     assert sorted(old_contacts, key=Contact.id_or_max) == sorted(new_contacts, key=Contact.id_or_max)
