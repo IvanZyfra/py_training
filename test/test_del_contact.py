8db2976e17a96e827263251f9880127b1a7bb71c
Ivan Polyakov <ivan.polyakov@zyfra.com>
2022-06-16 12:05:55 +0300

    Задание 13 (Реализованно случайное удаление элемента из списка контактов и групп)

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -1,8 +1,9 @@
 from model.contact import Contact
+from random import randrange
 
 
 
-def test_delete_first_contact(app):
+def test_delete_some_contact(app):
     if app.contact.count() == 0:
         app.contact.add_new_contact(Contact(first_name="Test", middle_name="Test", last_name="Test_name", nickname="", title="",
                 company="", address="", tel_home="", tel_mobile="",
@@ -10,8 +11,9 @@ def test_delete_first_contact(app):
                 email3="", homepage="", address2="", phone2="",
                 notes=""))
     old_contacts = app.contact.get_contacts_list()
-    app.contact.delete_first_contact()
+    index = randrange(len(old_contacts))
+    app.contact.delete_contact_by_index(index)
     assert len(old_contacts) - 1 == app.contact.count()
     new_contacts = app.contact.get_contacts_list()
-    old_contacts[0:1] = []
+    old_contacts[index:index+1] = []
     assert old_contacts == new_contacts
