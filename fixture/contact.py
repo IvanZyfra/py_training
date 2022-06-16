898494e7baf7c4e5b96ad855a114138fe11b8f07
Ivan Polyakov <ivan.polyakov@zyfra.com>
2022-06-16 12:06:29 +0300

    Revert "Задание 13 (Реализованно случайное удаление элемента из списка контактов и групп)"
    This reverts commit 8db2976e17a96e827263251f9880127b1a7bb71c.

--- fixture/contact.py
+++ fixture/contact.py
@@ -74,28 +74,20 @@ class ContactHelper:
         wd.find_element_by_link_text("home page").click()
 
     def delete_first_contact(self):
-        self.delete_contact_by_index(0)
-
-    def delete_contact_by_index(self, index):
         wd = self.app.wd
         # select first group
-        self.select_contact_by_index(index)
+        wd.find_element_by_name("selected[]").click()
         # submit deletion
         wd.find_element_by_xpath("//input[@value='Delete']").click()
         wd.switch_to.alert.accept()
         self.contact_cache = None
 
-    def select_contact_by_index(self, index):
-        wd = self.app.wd
-        wd.find_elements_by_name("selected[]")[index].click()
 
-    def modify_first_contact(self):
-        self.modify_contact_by_index(0)
 
-    def modify_contact_by_index(self, contact, index):
+    def modify_contact(self, contact):
         wd = self.app.wd
         # select first group
-        self.select_contact_by_index(index)
+        wd.find_element_by_name("selected[]").click()
         # click edit
         wd.find_element_by_xpath("//img[@title='Edit']").click()
         # modify contact
